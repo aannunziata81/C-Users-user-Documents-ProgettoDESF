a7291e64d5b1886a93628da081f0fd85f0d5aace
Antonio Annunziata <a.annunziata81@studenti.unisa.it>
2021-04-15 22:50:13 +0200

    aggiunto l'irraggiamento e upgrade main

--- main.m
+++ main.m
@@ -45,20 +45,29 @@ Soleggiamento_Gen = [0 0 0 0 0 0 0 16 103 239 325 340 227 249 132 23 0 0 0 0 0 0
 
 
 
-
-
-
-length(Soleggiamento_Gen)
-
-
 %ottobre - gennaio
 PloadH = [195 187 180 167 167.5 169.5 175 173 220 232 240 245 225 212 213 235 248 250 249.5 247.5 240 230 218 213]; %KW
 %febbraio-aprile e luglio-settembre
 PloadI = [85 82 81.8 75 74.8 74.9 64 74.3 102 118 121 120 100 77 103 112 114 121 120 105 104 105 102 101]; %KW
 %maggio-giugno
-PloadL = [15 15 15 14 14 14 8 7 7 7 7 7 7 7  7 7 7 7 7 7 8 12.5 13 13]; %KW
+PloadL = [15 15 15 14 14 14 8 7 7 7 7 7 7 7 7 7 7 7 7 7 8 12.5 13 13]; %KW
 
 p(1:24) = 0;
 
 figure(1)
 plot(ore, PloadL, ore, PloadI)
+
+% Batteria - PowerWall 2 - Tesla da 14 KWh - Costo € 6.000 
+% Scarica e carica batteria 5 KWh, per 10 secondi 7 KWh
+carica_scarica_per_ora = 5; % KWh
+
+Capacita_Batteria = 14; %KWh
+Numero_batterie = 1;
+
+% Range di mantenimento dello stato di carica
+% SOC - stato di carica
+SOCmax = 90; % percento
+SOCmin = 10; % percento
+
+
+
